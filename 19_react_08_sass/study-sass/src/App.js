caa23a34a4faaa98cea1429460d0f81d5e76f63c
sean-spreatics <sean@spreatics.com>
2022-12-06 11:40:21 +0900

    study: css & sass

---
+++ 19_react_08_sass/study-sass/src/App.js
@@ -0,0 +1,15 @@
+import Origin from './Origin';
+import Sass from './Sass';
+
+function App() {
+  return (
+    <div className="App">
+      <Origin />
+      <hr />
+
+      <Sass />
+    </div>
+  );
+}
+
+export default App;
